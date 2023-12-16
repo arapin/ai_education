284284cbba11fd2bc61f65f5156d5b451c9d01f6
MichaelJung <stormfiled@gmail.com>
2023-12-16 23:12:55 +0900

    - 배열 학습 - 그래프 학습

---
+++ education/matplotlibTest.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class pyplotTest:
+    def __init__(self):
+        print("init pyplotTest")
+
+    def plotTest(self):
+        # 데이터 준비
+        x = np.arange(0, 6, 0.1)
+        y = np.sin(x)
+
+        # 그래프 그리기
+        plt.plot(x, y)
+        plt.show()
+
+    def plotTest2(self):
+        # 데이터 준비
+        x = np.arange(0, 6, 0.1)
+        y1 = np.sin(x)
+        y2 = np.cos(x)
+
+        #그래프 그리기
+        plt.plot(x, y1, label="sin")
+        plt.plot(x, y2, linestyle="--", label="cos")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.title("sin & cos") # 제목
+        plt.legend()
+        plt.show()
+
+pt = pyplotTest()
+# pt.plotTest()
+pt.plotTest2()
--- education/numpyTest.py
+++ education/numpyTest.py
@@ -7,8 +7,32 @@ class NumpyTest:
 
     def numpy_test(self):
         x = np.array([1.0, 2.0, 3.0])
+        y = np.array([2.0, 4.0, 6.0])
+        print(x + y)
+        print(x - y)
+        print(x * y)
+        print(x / y)
+    def numpy_array_select(self):
+        x = np.array([[51, 55], [14, 19], [0, 4]])
         print(x)
+        print(x[0])
+        print(x[0][1])
 
+    def numpy_array_for(self):
+        x = np.array([[51, 55], [14, 19], [0, 4]])
+        for row in x:
+            print(row)
+
+    def numpy_array_flatten(self):
+        x = np.array([[51, 55], [14, 19], [0, 4]])
+        X = x.flatten()
+        print(X)
+        print(X[np.array([0, 2, 4])])
+        print(X > 15)
+        print(X[X>15])
 
 npTest = NumpyTest()
-npTest.numpy_test()
+# npTest.numpy_test()
+# npTest.numpy_array_select()
+# npTest.numpy_array_for()
+npTest.numpy_array_flatten()
