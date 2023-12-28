d7a359f86d584c803d5a4e9025d76d720ffbbed9
[HG372]정현수 <hsjung@hanatour.com>
2023-12-28 16:11:30 +0900

    - 학습알고리즘 구현

--- education/NeuralNetworkEducation.py
+++ education/NeuralNetworkEducation.py
@@ -1,7 +1,11 @@
+import sys, os
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
-import education.eduClass.TwoLayerNet as tlnc
+import matplotlib.pyplot as plt
+from education.eduClass.mnist import load_mnist
+from education.eduClass.TwoLayerNet import TwoLayerNet
 
-tln = tlnc.TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
+# tln = tlnc.TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
 # print(tln.params['W1'].shape)
 # print(tln.params['b1'].shape)
 # print(tln.params['W2'].shape)
@@ -11,11 +15,57 @@ tln = tlnc.TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
 # y = tln.predict(x)
 # print(y)
 
-x = np.random.rand(100, 784)
-t = np.random.rand(100, 10)
+# x = np.random.rand(100, 784)
+# t = np.random.rand(100, 10)
+#
+# grads = tln.numerical_gradient(x, t)
+# print(grads['W1'].shape)
+# print(grads['b1'].shape)
+# print(grads['W2'].shape)
+# print(grads['b2'].shape)
 
-grads = tln.numerical_gradient(x, t)
-print(grads['W1'].shape)
-print(grads['b1'].shape)
-print(grads['W2'].shape)
-print(grads['b2'].shape)
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+# 1에폭당 반복 수
+iter_per_epoch = max(train_size / batch_size, 1)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    # 1에폭당 정확도 계산
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train acc')
+plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
